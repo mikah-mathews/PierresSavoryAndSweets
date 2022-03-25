0da6b783eaa54aaa0315b646f2ee8edb0934b96a
Mikah Mathews <mikah@outofband.org>
2022-03-25 11:38:58 -0700

    Add flavor controllers and index, create, and details

---
+++ PierresSavoryAndSweet/Controllers/FlavorsController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using PierresSavoryAndSweet.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PierresSavoryAndSweet.Controllers
+{
+  public class FlavorsController : Controller
+  {
+    private readonly PierresSavoryAndSweetContext _db;
+
+    public FlavorsController(PierresSavoryAndSweetContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Flavors.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Flavor flavor)
+    {
+      _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisFlavor = _db.Flavors
+      .Include(flavor => flavor.JoinEntities)
+      .ThenInclude(join => join.Treat)
+      .FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
+  }
+}
